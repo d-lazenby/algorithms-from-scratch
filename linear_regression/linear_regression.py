7a8b8034cdc86b439711c85dd98f70549fbec0d2
Doug <douglazenby@gmail.com>
2023-09-22 07:58:03 +0100

    Modified names of class arguments to avoid repetition

--- linear_regression/linear_regression.py
+++ linear_regression/linear_regression.py
@@ -2,29 +2,35 @@ import numpy as np
 
 
 class LinearRegression:
-
-    def __init__(self, lr=0.001, n_iters=1000):
+    """
+    Perform linear regression using y_hat = theta0 + theta1 * X
+    """
+    def __init__(self, lr=0.001, n_iter=1000):
         self.lr = lr
-        self.n_iters = n_iters
-        self.weights = None
-        self.bias = None
+        self.n_iter = n_iter
+        self.theta1 = None
+        self.theta0 = None
 
-    def fit(self, X, y):
+    def fit(self, inputs, target):
         # init params
-        n_samples, n_features = X.shape
+        n_rows, n_features = inputs.shape
         # Initialize from zero
-        self.weights = np.zeros(n_features)
-        self.bias = 0
+        self.theta1 = np.zeros(n_features)
+        self.theta0 = 0
 
-        for _ in range(self.n_iters):
-            y_pred = np.dot(X, self.weights) + self.bias
+        for _ in range(self.n_iter):
+            y_hat = np.dot(inputs, self.theta1) + self.theta0
 
-            dw = (1 / n_samples) * np.dot(X.T, (y_pred - y))
-            db = (1 / n_samples) * np.sum(y_pred - y)
+            d_theta1 = (1 / n_rows) * np.dot(inputs.T, (y_hat - target))
+            d_theta0 = (1 / n_rows) * np.sum(y_hat - target)
 
-            self.weights -= self.lr * dw
-            self.bias -= self.lr * db
+            self.theta1 -= self.lr * d_theta1
+            self.theta0 -= self.lr * d_theta0
 
-    def predict(self, X):
-        y_pred = np.dot(X, self.weights) + self.bias
-        return y_pred
+    def predict(self, inputs):
+        predictions = np.dot(inputs, self.theta1) + self.theta0
+        return predictions
+
+
+def mse(predictions, y_true):
+    return np.mean((y_true - predictions) ** 2)
