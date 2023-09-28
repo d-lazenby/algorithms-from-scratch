c5b3b1b782b4a523a51dfa48010b7f916e03c43f
Doug <douglazenby@gmail.com>
2023-09-28 21:40:39 +0100

    Code for Logistic Regression
    Added code for implementation and testing

---
+++ logistic_regression/logistic_regression.py
@@ -0,0 +1,46 @@
+import numpy as np
+
+
+class LogisticRegression:
+    """
+    Perform logistic regression using y_hat = 1 / (1 + exp(-(theta0 + theta1*X)))
+    """
+
+    def __init__(self, lr=0.001, n_iter=1000):
+        self.lr = lr
+        self.n_iter = n_iter
+        self.theta1 = None
+        self.theta0 = None
+
+    def fit(self, inputs, target):
+        # init params
+        n_rows, n_features = inputs.shape
+        # Initialize from zero
+        self.theta1 = np.zeros(n_features)
+        self.theta0 = 0
+
+        for _ in range(self.n_iter):
+            thetaX = np.dot(inputs, self.theta1) + self.theta0
+            y_hat = sigmoid(thetaX)
+
+            d_theta1 = (1 / n_rows) * np.dot(inputs.T, (y_hat - target))
+            d_theta0 = (1 / n_rows) * np.sum(y_hat - target)
+
+            self.theta1 -= self.lr * d_theta1
+            self.theta0 -= self.lr * d_theta0
+
+    def predict(self, inputs):
+        thetaX = np.dot(inputs, self.theta1) + self.theta0
+        predictions = sigmoid(thetaX)
+        predictions = np.where(predictions >= 0.5, 1, 0)
+        return predictions
+
+
+def sigmoid(x):
+    return 1 / (1 + np.exp(-x))
+
+
+def accuracy(y_true, y_pred):
+    acc = np.sum(y_true == y_pred) / len(y_pred)
+    return round(acc, 4)
+
---
+++ logistic_regression/logistic_regression_test.py
@@ -0,0 +1,17 @@
+import numpy as np
+from sklearn.model_selection import train_test_split
+from sklearn import datasets
+import matplotlib.pyplot as plt
+
+from logistic_regression import LogisticRegression, accuracy
+
+dataset = datasets.load_breast_cancer()
+X, y = dataset.data, dataset.target
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=1234)
+
+model = LogisticRegression(lr=0.0001)
+model.fit(X_train, y_train)
+predictions = model.predict(X_test)
+
+print(f"Accuracy: {accuracy(y_test, predictions)}")
