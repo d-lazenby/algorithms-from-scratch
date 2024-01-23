993d151c6dea798890fc88365e6ebfb6e43e481a
Doug <douglazenby@gmail.com>
2024-01-23 21:39:34 +0000

    Updated notes Tidied code and outputted graphs of results

--- logistic_regression/logistic_regression.md
+++ logistic_regression/logistic_regression.md
@@ -1,6 +1,6 @@
 # Logistic Regression
 
-In binary classification, we want to separate our data into two classes that we'll label 0 and 1. Examples of this include, among many others, classification of tumours as malign or benign or of emails as spam or not spam. To be concrete, given an input $x^{(i)}$, $i\in\{0,1,2,\ldots,m\}$, we wish to predict a corresponding label $y^{(i)}$, where $y^{(i)}\in\{0,1\}$. 
+In binary classification, we want to separate our data into two classes that we'll label 0 and 1. Examples of this include, among many others, classification of tumours as malign or benign or of emails as spam or not spam. To be concrete, for each input $x^{(i)}$, $i\in\{0,1,2,\ldots,m\}$, we wish to predict a corresponding label $y^{(i)}$, where $y^{(i)}\in\{0,1\}$. 
 
 Given we know that $y\in\{0,1\}$ it makes sense to modify our hypothesis to reflect this. There are many functions that would do the job, but logistic regression uses the sigmoid function (also known as the logistic function), defined as 
 $$
@@ -20,20 +20,20 @@ since if an instance is labelled 1 it can't be labelled 0, and vice versa. This 
 $$
 p(y\mid x;\theta) = h_\theta(x)^y(1-h_\theta(x)^{1-y}),
 $$
-which is the distribution of y given x, parameterised by $\theta$. Assuming the training examples were generated independently, the likelihood is given by
+which is the distribution of $y$ given $x$, parameterised by $\theta$. Assuming the training examples were generated independently, the likelihood is given by
 $$
 L(\theta) = p(\vec{y}\mid X;\theta) = \prod_{i=1}^{m}h(x)^{y}(1-h(x))^{1-y},
 $$
-where $i$ indexes each of the $x$ and $y$ in the product.
+where $i$ indexes each of the $x$ and $y$ in the product. Given inputs $X$ we'd like to maximise the likelihood. In general a sum is easier to deal with than a product so in practice we actually take logs of $L$ to form a sum of logarithms and maximise this instead (this works since the logarithm is a monotonically increasing function, so maximising $\log f(x)$ is equivalent to maximising $f(x)$).
 
 Taking logs so that $\ell (\theta) = \log L(\theta)$ and differentiating $\ell$ with respect to a single parameter $\theta_j$ for a single training example, we can show that
 $$
 \frac{\partial \ell}{\partial\theta_j} = (y - g(\theta^Tx))x_j = (y - h(x))x_j,
 $$
-leading to the stochastic gradient ascent (*ascent* because we want to maximise the log-likelihood) rule
+leading to the following update rule for $\theta$:
 $$
 \theta_j := \theta_j + \alpha (y^{(i)} - h_\theta(x^{(i)}))x_j^{(i)}.
 $$
 
 ## Testing
-Cancer dataset data split: (array([0, 1]), array([212, 357]))
+To test the algorithm, we can use a toy dataset built from the scikit-learn library that consists of 500 data points representing two features and two classes. This is split into training and test sets and fed into the algorithm to obtain a decision boundary. This recovered 
--- logistic_regression/logistic_regression_test.py
+++ logistic_regression/logistic_regression_test.py
@@ -7,41 +7,60 @@ import matplotlib.pyplot as plt
 
 from logistic_regression import LogisticRegression, accuracy
 
-# Compute decision boundary
-def compute_decision_boundary(X, model):
-    return (model.threshold - model.theta0 - model.theta_j[1] * X ) / model.theta_j[0]
+def compute_decision_boundary(X2, model):
+    return (model.threshold - model.theta0 - model.theta_j[1] * X2 ) / model.theta_j[0]
 
-X, y = make_blobs(n_samples=500, n_features=2, centers=2, cluster_std=0.9, random_state=0)
+def plot_results(Xtrain, ytrain, Xtest, ytest, model, accuracy=None, save=None):
+    colors = {0:'red', 1:'blue'}
+    fig, ax = plt.subplots(figsize=(10,4), ncols=2)
+
+    for i, ml_set in enumerate(((Xtrain, ytrain), (Xtest, ytest))):
+        print("Index is ", i)
+        df = pd.DataFrame(dict(X1=ml_set[0][:,0], X2=ml_set[0][:,1], y=ml_set[1]))
+        db = compute_decision_boundary(ml_set[0][:,1], model)
+        print(f"Db computed fine for index {i}")
+        blobs = df.groupby('y')
+
+        for key, group in blobs:
+            group.plot(ax=ax[i], kind='scatter', x='X1', y='X2', label=key, color=colors[key], alpha=0.5)
+
+    # Plot linear model
+        ax[i].plot(db, ml_set[0][:,1], '-', label=r'$h_\theta$')
+        ax[i].legend()
+
+    ax[0].set_title("Train")
+    if accuracy:
+        ax[1].set_title(f"Test. Accuracy {accuracy}")
+    else:
+        ax[1].set_title(f"Test")
+
+    if save:
+        plt.savefig(f'./logistic_regression/{save}.png')
+    
+    plt.show()
+
+
+# random_state = 8
+# X, y = make_blobs(n_samples=500, n_features=2, centers=2, cluster_std=2.0, random_state=12)
+X, y = make_classification(n_samples=500, n_features=2, n_informative=2, 
+                           n_redundant=0, n_repeated=0, n_classes=2, n_clusters_per_class=1, 
+                           class_sep=0.5, random_state=5)
+# sklearn.datasets.make_classification(n_samples=100, n_features=20, *, 
+#                                      n_informative=2, n_redundant=2, n_repeated=0, 
+#                                      n_classes=2, n_clusters_per_class=2, weights=None, 
+#                                      flip_y=0.01, class_sep=1.0, 
+#                                      hypercube=True, shift=0.0, scale=1.0, shuffle=True, 
+#                                      random_state=None)[source]
 
 # dataset = datasets.load_breast_cancer()
 # X, y = dataset.data, dataset.target
-
-# print(np.unique(y, return_counts=True))
-
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=32)
-df_train = pd.DataFrame(dict(X1=X_train[:,0], X2=X_train[:,1], y=y_train))
-df_test = pd.DataFrame(dict(X1=X_test[:,0], X2=X_test[:,1], y=y_test))
 
 model = LogisticRegression(lr=0.01)
 model.fit(X_train, y_train)
 predictions = model.predict(X_test)
+acc = accuracy(y_test, predictions)
 
-print(f"Accuracy: {accuracy(y_test, predictions)}")
+print(f"Accuracy: {acc}")
 
-db_train = compute_decision_boundary(X_train[:,1], model)
-db_test = compute_decision_boundary(X_test[:,1], model)
-
-colors = {0:'red', 1:'blue'}
-fig, ax = plt.subplots(ncols=2)
-blobs_train = df_train.groupby('y')
-for key, group in blobs_train:
-    group.plot(ax=ax[0], kind='scatter', x='X1', y='X2', label=key, color=colors[key], alpha=0.5)
-
-blobs_test = df_test.groupby('y')
-for key, group in blobs_test:
-    group.plot(ax=ax[1], kind='scatter', x='X1', y='X2', label=key, color=colors[key], alpha=0.5)
-
-# Plot linear model
-ax[0].plot(db_train, X_train[:,1], '-')
-ax[1].plot(db_test, X_test[:,1], '-')
-plt.show()
+plot_results(X_train, y_train, X_test, y_test, model, accuracy=acc)
